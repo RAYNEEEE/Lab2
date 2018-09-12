60287ead75a54ae7cfa797a6a602134fa3a50e5b
rayne <runqixu2@illinois.edu>
2018-09-12 18:53:28 -0500

    111

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,23 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            if (maze.canMove()) ;{
+                maze.move();
+            }else{
+                maze.turnRight();
+                if(maze.canMove()){
+                    maze.move();
+                }else{
+                    maze.turnLeft();
+                }
+            }
+
+        }
+
+    }
+
+
+    // Implement your maze solving algorithm here
         }
 
         if (maze.isFinished()) {
